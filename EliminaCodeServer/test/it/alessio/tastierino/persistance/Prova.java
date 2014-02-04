3c49f22ca6ea03f46335d5e230fc6de1e588b591
Alessio De Angelis <ale.deangelis89@gmail.com>
2014-02-04 13:30:53 +0100

    ignore

---
+++ EliminaCodeServer/test/it/alessio/tastierino/persistance/Prova.java
@@ -0,0 +1,10 @@
+package it.alessio.tastierino.persistance;
+
+public class Prova {
+	public static void main(String[] args) {
+		// os.name
+		String s = System.getProperty("os.arch");
+	        System.out.println(s);
+	        System.getProperties().list(System.out);
+	}
+}
